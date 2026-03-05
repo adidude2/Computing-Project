1eb2fd8d699334a55b4c2a1157397cc0f7f2e152
Aditaya Choudhury <adidude203@gmail.com>
2026-03-05 02:08:53 +0000

    MUAH

--- Disk with varied vels.py
+++ Disk with varied vels.py
@@ -48,12 +48,6 @@ def Posallocate(N, Rd, Rmax, M, z0):
     masses =  np.full(N, M/N)# 1e25/N)
     return Points, masses
 
-
-#a,mass = Posallocate(1000, 1000, 100000)
-#Xmatrix = a[:,0]
-#Ymatrix = a[:,1]
-#Zmatrix = a[:,2]
-
 def HaloAccel(arrx, arry, arrz, Mhalo, Rvir, c):
     
     r = np.sqrt(arrx**2 + arry**2 + arrz**2)
@@ -89,10 +83,6 @@ def HaloAccel(arrx, arry, arrz, Mhalo, Rvir, c):
     az = amag * arrz / r_safe
     return ax, ay, az, Menc, Rs, rho0
 
-    
-#a = HaloAccel(Xmatrix, Ymatrix, Zmatrix, 4, 2)
-
-
 def force_vectorised(arrx, arry, arrz, mass):
     
     dx = arrx[:, None] - arrx[None, :]
@@ -115,10 +105,6 @@ def force_vectorised(arrx, arry, arrz, mass):
     # Net force on each particle
     #return arrx,arry
     return Fx.sum(axis=1), Fy.sum(axis=1), Fz.sum(axis=1)
-
-
-#print(force_vectorised(a[:,0],a[:,1],a[:,2], mass))
-
 
 def AccelCalc(arrx,arry, arrz, mass, t, Mhalo, Rvir, c):
     Fx, Fy, Fz = force_vectorised(arrx, arry, arrz, mass)
@@ -147,19 +133,12 @@ def AccelCalc(arrx,arry, arrz, mass, t, Mhalo, Rvir, c):
     dvz = az * t
     return dvx, dvy, dvz, M
 
-#print(AccelCalc(Xmatrix, Ymatrix,odt))
-
 def COMCalc(arrx,arry, arrz, mass):
     ms = mass
     COMx = (np.sum(ms * arrx))/(np.sum(ms))
     COMy = (np.sum(ms * arry))/(np.sum(ms))
     COMz = (np.sum(ms * arrz))/(np.sum(ms))
     return COMx, COMy, COMz
-    
-    
-#x,y = COMCalc(Xmatrix, Ymatrix)
-#print(x, y)
-
 
 def HaloVels(a, Menc, mass):
     Xmatrix = a[:,0]
@@ -230,8 +209,6 @@ def BetterHaloVels(a, t, dvx, dvy, dvz, mass, f, fz):
     vels = np.column_stack((velsxy,vz))
     
     return vels
-
-
 
 def HugeFunc(T, t, ass, mass, Mhalo, Rvir, c, f, fz):
     a = ass
@@ -312,7 +289,7 @@ def HugeFunc(T, t, ass, mass, Mhalo, Rvir, c, f, fz):
         VXmatrix += 0.5 * (dvx + dvx_new)
         VYmatrix += 0.5 * (dvy + dvy_new)
         VZmatrix += 0.5 * (dvz + dvz_new)
-    return SavedCOM, SavedX, SavedY, SavedZ, SavedVX, SavedVY, SavedVZ, mass, SavedSteps, Rs, rho0, SavedMenc
+    return SavedCOM, SavedX, SavedY, SavedZ, SavedVX, SavedVY, SavedVZ, mass, SavedSteps, Rs, rho0
 
 def plotfig(R, SavedCOM, SavedX, SavedY, SavedZ, SavedSteps, t, view, filename):
     
@@ -384,124 +361,10 @@ def plotfig(R, SavedCOM, SavedX, SavedY, SavedZ, SavedSteps, t, view, filename):
     
     return ani
 
-
-def EnergyPlot(T, t, a, b, R, Mhalo, Rvir, c, f, fz):
-    #fig, axes = plt.subplots(2, 1, figsize=(9, 7))
-    fig = plt.figure(figsize=(7, 5))
-    ax = fig.add_subplot()
-    SavedCOM, SavedX, SavedY, SavedZ, Vx, Vy, Vz, mass, SavedSteps, Rs, rho0, SavedMenc = HugeFunc(T, t, a, b, Mhalo, Rvir, c, f, fz)
-    ani_face = plotfig(R, SavedCOM, SavedX, SavedY, SavedZ, SavedSteps, t,
-                     view = "face",
-                     filename="disk_faceon.mp4")
-    ani_edge = plotfig(R, SavedCOM, SavedX, SavedY, SavedZ, SavedSteps, t,
-                     view = "edge",
-                     filename="disk_edgeon.mp4")
-    #ani_inclined = plotfig(R, SavedCOM, SavedX, SavedY, SavedZ, SavedSteps, t,
-    #                 view = "inclined",
-    #                 filename="disk_incline.mp4")
-    ms = mass
-    X = SavedX
-    Y = SavedY
-    Z = SavedZ
-    Np = len(X[0])
-    halfposX = np.empty(Np)
-    halfposY = np.empty(Np)
-    halfposZ = np.empty(Np)
-    #Uarr = np.empty(N*(N-1)/2)
-    Utotarr = np.empty(len(X)-1)
-    KEtotarr = np.empty(len(X)-1)
-    Virtotarr = np.empty(len(X)-1)
-    for i in range (0,len(X)-1):
-        x = X[i]
-        y = Y[i]
-        z = Z[i]
-        Utot = 0
-        #dx = x[:,None] - x[None,:]
-        #dy = y[:,None] - y[None,:]
-        #dz = z[:,None] - z[None,:]
-        #r = np.sqrt(dx**2 + dy**2 + dz**2 + e**2)
-        Utot = 0
-        midX = 0.5 * (X[i] + X[i+1])
-        midY = 0.5 * (Y[i] + Y[i+1])
-        midZ = 0.5 * (Z[i] + Z[i+1])
-        
-        positions = np.stack([midX, midY, midZ], axis=1)
-        #positions = np.stack([midX, midY, midZ], axis=1)
-        d = positions[:,None,:] - positions[None,:,:]
-        r = np.sqrt(np.sum(d*d,axis=2)+e**2)
-        U = -G * (ms[:,None]*ms[None,:]) / r
-        np.fill_diagonal(U,0)
-        
-        r = np.sqrt(x**2 + y**2 + z**2)
-        r_safe = np.where(r==0, 1e-10, r)
-        
-        phihalo = (-4 * np.pi * G * rho0 * Rs**3 / r_safe) * np.log(
-            
-             1 + r_safe/Rs
-            
-             )
-        
-        Uhalo   = mass * phihalo
-        
-        U_stellar = np.sum(U) / 2
-        
-        Utot = U_stellar + np.sum(Uhalo)
-        Utotarr[i] = Utot
-        
-        hax, hay, haz, _, _, _ = HaloAccel(x, y, z, Mhalo, Rvir, c)
-        Omega_halo = np.sum(ms * (x * hax + y * hay + z * haz))
-        
-        Ktot = 0
-        vx = Vx[i+1]
-        vy = Vy[i+1]
-        vz = Vz[i+1]
-        v2 = vx**2 + vy**2 + vz**2
-        Ktot = 0.5 * np.sum(ms * v2)
-        KEtotarr[i] = Ktot
-        
-        Vir = 0
-        Vir = (2*Ktot + Utot)
-        # Vir = 2K + Omega_stellar + Omega_halo; for gravity Omega_stellar = U_stellar
-        Vir = 2 * Ktot + U_stellar + Omega_halo
-        Virtotarr[i] = Vir
-    Etot = Utotarr + KEtotarr
-    Uavg = np.mean(Utotarr)
-    Kavg = np.mean(KEtotarr)
-    Tavg = np.mean(Etot)
-    Vavg = np.mean(Virtotarr)
-    
-    E0   = Etot[0]
-    Vir0 = Virtotarr[0]
-    
-    de   = 100 * (Etot - E0) / E0
-    
-    max_de  = np.max(np.abs(de))
-    mean_de = np.mean(np.abs(de))
-    
-    
-    
-    vir_ratio = np.abs(Virtotarr) / np.abs(Utotarr)
-
-    max_dvir  = 100 * np.max(vir_ratio)
-    mean_dvir = 100 * np.mean(vir_ratio)
-    
-    seconds_per_year = 1e6 * 3600 * 24 * 365
-    x = (np.array(SavedSteps[:-1]) * t) / seconds_per_year
-    ax.plot(x, Utotarr, label = 'Potential Energy', color = 'tab:red')
-    ax.plot(x, KEtotarr, label = 'Kinetic Energy', color = 'tab:purple')
-    ax.plot(x, Etot, label = 'Total Energy', color = 'tab:gray')
-    ax.plot(x, Virtotarr, label = 'Virial Energy', color = 'tab:blue')
-    ax.set_xlabel("Time (Myrs)")
-    ax.set_ylabel("Energy (J)")
-    ax.legend()
-    fig.savefig(r'C:\Users\adidu\Documents\Work stuff\Year 3\Computing Project\Old Python Files\Energy Deviation from meanNbody.png', transparent=True)
-    return Uavg, Kavg, Tavg, vir_ratio, max_de, max_dvir, mean_de, mean_dvir, ani_face, ani_edge, fig
-
-def EnergyPlotfrac(T, t, a, b, R, Mhalo, Rvir, c, f, fz):
-    fig, axes = plt.subplots(2, 1, figsize=(9, 7))
+def EnergyPlotfrac(t, R, Mhalo, Rvir, c, SavedCOM, SavedX, SavedY, SavedZ, Vx, Vy, Vz, mass, SavedSteps, Rs, rho0):
+    fig, axes = plt.subplots(3, 1, figsize=(9, 10))
     #fig = plt.figure(figsize=(7, 5))
     #ax = fig.add_subplot()
-    SavedCOM, SavedX, SavedY, SavedZ, Vx, Vy, Vz, mass, SavedSteps, Rs, rho0, SavedMenc = HugeFunc(T, t, a, b, Mhalo, Rvir, c, f, fz)
     ani_face = plotfig(R, SavedCOM, SavedX, SavedY, SavedZ, SavedSteps, t,
                      view = "face",
                      filename="disk_faceon.mp4")
@@ -511,23 +374,21 @@ def EnergyPlotfrac(T, t, a, b, R, Mhalo, Rvir, c, f, fz):
     #ani_inclined = plotfig(R, SavedCOM, SavedX, SavedY, SavedZ, SavedSteps, t,
     #                 view = "inclined",
     #                 filename="disk_incline.mp4")
+    
     ms = mass
     X = SavedX
     Y = SavedY
     Z = SavedZ
     Np = len(X[0])
-    halfposX = np.empty(Np)
-    halfposY = np.empty(Np)
-    halfposZ = np.empty(Np)
     #Uarr = np.empty(N*(N-1)/2)
     Utotarr = np.empty(len(X)-1)
     KEtotarr = np.empty(len(X)-1)
     Virtotarr = np.empty(len(X)-1)
+    Lztot = np.empty(len(X)-1)
     for i in range (0,len(X)-1):
         x = X[i]
         y = Y[i]
         z = Z[i]
-        Utot = 0
         #dx = x[:,None] - x[None,:]
         #dy = y[:,None] - y[None,:]
         #dz = z[:,None] - z[None,:]
@@ -563,7 +424,6 @@ def EnergyPlotfrac(T, t, a, b, R, Mhalo, Rvir, c, f, fz):
         hax, hay, haz, _, _, _ = HaloAccel(x, y, z, Mhalo, Rvir, c)
         Omega_halo = np.sum(ms * (x * hax + y * hay + z * haz))
         
-        Ktot = 0
         vx = Vx[i+1]
         vy = Vy[i+1]
         vz = Vz[i+1]
@@ -572,10 +432,14 @@ def EnergyPlotfrac(T, t, a, b, R, Mhalo, Rvir, c, f, fz):
         KEtotarr[i] = Ktot
         
         Vir = 0
-        Vir = (2*Ktot + Utot)
         # Vir = 2K + Omega_stellar + Omega_halo; for gravity Omega_stellar = U_stellar
         Vir = 2 * Ktot + U_stellar + Omega_halo
         Virtotarr[i] = Vir
+        
+        Lz = 0
+        Lz = np.sum(ms*(x*vy-y*vx))
+        Lztot[i] = Lz
+
     Etot = Utotarr + KEtotarr
     Uavg = np.mean(Utotarr)
     Kavg = np.mean(KEtotarr)
@@ -584,49 +448,118 @@ def EnergyPlotfrac(T, t, a, b, R, Mhalo, Rvir, c, f, fz):
     du = np.empty(len(Utotarr))
     dk = np.empty(len(Utotarr))
     dTot = np.empty(len(Utotarr))
+    dLz = np.empty(len(Utotarr))
+    dVir = np.empty(len(Utotarr))
     for i in range (0, len(Utotarr)):
-        du[i] = (Utotarr[i] - Utotarr[0])/abs(Utotarr[0])
-        dk[i] = (KEtotarr[i] - KEtotarr[0])/abs(KEtotarr[0])
+        du[i]   = (Utotarr[i] - Utotarr[0])/abs(Utotarr[0])
+        dk[i]   = (KEtotarr[i] - KEtotarr[0])/abs(KEtotarr[0])
         dTot[i] = (Etot[i] - Etot[0])/abs(Etot[0])
-        
+        dVir[i] = np.abs(Virtotarr[i]) / np.abs(Utotarr[0])
+        dLz[i]  = (Lztot[i] - Lztot[0])/abs(Lztot[0])
+    
+    max_du   = np.max(np.abs(du))
+    max_dk   = np.max(np.abs(dk))
+    max_de   = np.max(np.abs(dTot))
+    max_dvir = np.max(np.abs(dVir))
+    max_dlz  = np.max(np.abs(dLz))
         
     E0   = Etot[0]
-    Vir0 = Virtotarr[0]
-    
     de   = 100 * (Etot - E0) / E0
     
-    max_de  = np.max(np.abs(de))
     mean_de = np.mean(np.abs(de))
     
     
     
-    vir_ratio = np.abs(Virtotarr) / np.abs(Utotarr)
 
-    max_dvir  = 100 * np.max(vir_ratio)
-    mean_dvir = 100 * np.mean(vir_ratio)
+    #mean_dvir = 100 * np.mean(vir_ratio)
     
     seconds_per_year = 1e6 * 3600 * 24 * 365
     x = (np.array(SavedSteps[:-1]) * t) / seconds_per_year
-    axes[0].plot(x, Utotarr, label = 'Potential Energy', color = 'tab:red')
-    axes[0].plot(x, KEtotarr, label = 'Kinetic Energy', color = 'tab:purple')
-    axes[0].plot(x, Etot, label = 'Total Energy', color = 'tab:gray')
-    axes[1].plot(x,du, label = r'$\Delta E_{pot}$', color = 'green')
-    axes[1].plot(x,dk, label = r'$\Delta E_{kin}$', color = 'orange')
-    axes[1].plot(x,dTot, label = r'$\Delta E_{tot}$', color = 'purple')
-    axes[0].plot(x, Virtotarr, label = 'Virial Energy', color = 'tab:blue')
-    axes[0].set_xlabel("Time (Myrs)")
+    axes[0].plot(x, Utotarr, label = '$U$', color = 'green')
+    axes[0].plot(x, KEtotarr, label = '$K$', color = 'orange')
+    axes[0].plot(x, Etot, label = '$E$', color = 'purple')
+    axes[0].plot(x, Virtotarr, label = '$2K + U$', color = 'tab:blue')
+    axes[1].plot(x,du, label = r'$\Delta U/U_0$', color = 'green')
+    axes[1].plot(x,dk, label = r'$\Delta K/K_0$', color = 'orange')
+    axes[1].plot(x,dTot, label = r'$\Delta E/E_0$', color = 'purple')
+    axes[1].plot(x,dVir, label = r'$(2K+U)/|U_0|$', color = 'tab:blue')
+    axes[2].plot(x, dLz, label = r'$\Delta L_z/L_{z0}$', color = 'black')
+    axes[0].tick_params(axis='x', labelbottom=False)
+    axes[1].tick_params(axis='x', labelbottom=False)
+    axes[2].set_xlabel("Time (Myrs)")
     axes[0].set_ylabel("Energy (J)")
-    axes[0].legend()
-    axes[1].legend()
+    axes[1].set_ylabel(r'$\frac{\Delta E}{|E_0|}$', rotation='horizontal',  fontsize=14, labelpad=10)
+    axes[2].set_ylabel(r'$\frac{\Delta L_z}{|L_{z0}|}$', rotation='horizontal',  fontsize=14, labelpad=14)
+    axes[0].legend(fontsize=9)
+    axes[1].legend(fontsize=9)
+    #axes[2].legend()
     fig.savefig(r'C:\Users\adidu\Documents\Work stuff\Year 3\Computing Project\Old Python Files\Energy Deviation from meanNbody.png', transparent=True)
-    return Uavg, Kavg, Tavg, vir_ratio, max_de, max_dvir, mean_de, mean_dvir, ani_face, ani_edge, fig
+    return Uavg, Kavg, Tavg, max_du, max_dk, max_de, max_dvir, max_dlz, ani_face, ani_edge, fig
 
+def Toomre(X, Y, Vx, Vy):
+    x = X[0]
+    y = Y[0]
+    vx = Vx[0]
+    vy = Vy[0]
+    R = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(Y[0], X[0])
+    
+    Vr = (x*vx + y*vy)/R
+    vphi = (x*vy - y*vx)/R
+    
+    
+    nbins = 40
+    R_bins = np.linspace(0, Rmax, nbins+1)
+    R_centers = 0.5*(R_bins[:-1] + R_bins[1:])
+    
+    Sigma = np.zeros(nbins)
+    
+    for i in range(nbins):
+        mask = (R >= R_bins[i]) & (R < R_bins[i+1])
+        Mbin = np.sum(mass[mask])
+        area = np.pi * (R_bins[i+1]**2 - R_bins[i]**2)
+        
+        Sigma[i] = Mbin / area
+    
+    sigma_R = np.zeros(nbins)
 
+    for i in range(nbins):
+
+        mask = (R >= R_bins[i]) & (R < R_bins[i+1])
+
+        if np.sum(mask) > 1:
+            sigma_R[i] = np.std(Vr[mask])
+    
+    
+    vphi_mean = np.zeros(nbins)
+
+    for i in range(nbins):
+
+        mask = (R >= R_bins[i]) & (R < R_bins[i+1])
+
+        if np.sum(mask) > 0:
+            vphi_mean[i] = np.mean(vphi[mask])
+    
+    Omega = vphi_mean / R_centers
+    Omega2 = Omega**2
+    dOmega2_dR = np.gradient(Omega2, R_centers)
+
+    kappa = np.sqrt(4*Omega2 + R_centers*dOmega2_dR)
+    
+    Q = sigma_R * kappa / (3.36 * G * Sigma)
+    
+    plt.figure(figsize=(7, 5))
+    plt.plot(R_centers, Q)
+    plt.axhline(1, linestyle='--')
+    plt.axhline(2, linestyle=':')
+    
+    
+    
 Msol            = 1.989e+30
 Kpc             = 3.086e+19
 Myr             = 3600 * 24 * 365 * 1e6
 
-N               = 1000              # Number of bodies
+N               = 1500              # Number of bodies
 Rd              = 1.3 * 2.15 * Kpc  # Scale length
 Rmax            = 5 * Rd            # Truncation Radius
 z0              = 0.3 * Kpc         # Thickness of the disk
@@ -644,7 +577,7 @@ sigma_z_factor  = 0.1               # vertical velocity dispersion
 
 
 odt = 0.1 * Myr                     # Timestep
-T_orbit = 212 * Myr * 1             # Simulation time
+T_orbit = 212 * Myr * 0.1             # Simulation time
 
 oT = T_orbit
 oTotT = oT / odt
@@ -657,19 +590,57 @@ Z = a[:,2]
 
 
 #ani, SavedX = plotfig(oTotT,  odt, R, a, b)
-U, KE, E, Vir, max_de, max_dvir, mean_de, mean_dvir, ani_face, ani_edge, fig = EnergyPlotfrac(oTotT, odt , a, b, Rmax, Mhalo_effective, Rvir, c, sigma_R_factor, sigma_z_factor)
+SavedCOM, SavedX, SavedY, SavedZ, Vx, Vy, Vz, mass, SavedSteps, Rs, rho0 = HugeFunc(
+   
+    oTotT,
+    odt,
+    a,
+    b,
+    Mhalo_effective,
+    Rvir,
+    c,
+    sigma_R_factor,
+    sigma_z_factor
+    
+    )
 
-print("Mean Kinetic:", KE)
-print("Mean Potential:", U)
-print("Mean Total:", E)
-print("Mean Virial:", Vir)
-print("Max Energy Percentage Difference:", max_de)
-print("Max Virial Percentage Difference:", max_dvir)
-print("Mean Energy Percentage Difference:", mean_de)
-print("Mean Virial Percentage Difference:", mean_dvir)
+Uavg, Kavg, Tavg, max_du, max_dk, max_de, max_dvir, max_dlz, ani_face, ani_edge, fig = EnergyPlotfrac(
+     
+    odt,
+    Rmax,
+    Mhalo_effective,
+    Rvir,
+    c,
+    SavedCOM,
+    SavedX,
+    SavedY,
+    SavedZ,
+    Vx,
+    Vy,
+    Vz,
+    mass,
+    SavedSteps,
+    Rs,
+    rho0
+    
+    )
+
+print("Mean Kinetic:", Kavg)
+print("Mean Potential:", Uavg)
+print("Mean Total:", Tavg)
+print("Max Energy Fractional Difference:", max_de)
+print("Max Kinetic Fractional Difference:", max_dk)
+print("Max Potential Fractional Difference:", max_du)
+print("Max Virial Residue:", max_dvir)
+print("Max Angular Momentum Fractional Difference:", max_dlz)
+#print("Mean Energy Percentage Difference:", mean_de)
+#print("Mean Virial Percentage Difference:", mean_dvir)
 
 #print(SavedX)
 #rhor = HaloAccel(Xmatrix, Ymatrix, Zmatrix, Mhalo, Rvir, c)
+
+
+Toomre(SavedX, SavedY, Vx, Vy)
 
 
 
